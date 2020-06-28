0dca551e55965d19719897fc4dae5760b138e7fe
Dent24 <cljhs90607@gmail.com>
2020-06-28 15:53:14 +0800

    js增加視窗轉動

--- counter.js
+++ counter.js
@@ -1,6 +1,15 @@
 $(document).ready(function () {
-    var offset = $("#test").offset();
-    var top = offset.top;
-    var left = offset.left;
+    
+    $(window).scroll(function () { 
+        var windowTop = $(window).scrollTop();
+
+        var offset = $("#test").offset();
+        var top = offset.top;
+        var left = offset.left;
+
+        console.log("元素" + top);
+        console.log("視窗" + windowTop);
+        
+    });
     
 });
